24b663ec7d70155bd03670be15ec8d2119a2fba6
MinjunISSAC <qkralswns931026@gmail.com>
2021-01-29 21:49:39 +0900

    Math Puzzle first commit.

---
+++ Assets/AdmobScreenAd.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+public class AdmobScreenAd : MonoBehaviour
+{
+    private readonly string unitID = "ca-app-pub-2795477849017803/1188974967";
+    private readonly string test_unitID = "ca-app-pub-2795477849017803/1188974967";
+
+    private readonly string test_deviceID = "";
+
+    private InterstitialAd screenAd;
+
+    private void Start()
+    {
+        InitAd();
+        Invoke("Show", 10f);
+    }
+    private void InitAd()
+    {
+        string id = unitID;
+
+        screenAd = new InterstitialAd(id);
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        screenAd.LoadAd(request);
+        screenAd.OnAdClosed += (sender, e) => Debug.Log("광고가 닫힘");
+        screenAd.OnAdLoaded += (sender, e) => Debug.Log("광고가 로드됨");
+    }
+
+    public void Show()
+    {
+        StartCoroutine("ShowScreenAd");
+    }
+
+    private IEnumerator ShowScreenAd()
+    {
+        while (!screenAd.IsLoaded())
+        {
+            yield return null;
+        }
+        screenAd.Show();
+    }
+}
---
+++ Assets/coolTime.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class coolTime : MonoBehaviour
+{
+    public Image coolTimeSlider;
+    public Text coolTimeText;
+
+    private bool canSlider;
+    private float coolTimedownTime;
+
+    private float updateTime;
+
+    void Start()
+    {
+        canSlider = true;
+        updateTime = 0.0f;
+        coolTimedownTime = 60.0f;
+    }
+
+    void Update()
+    {
+        if (canSlider)
+        {
+            updateTime = updateTime + Time.deltaTime;
+            coolTimeSlider.fillAmount = 1.0f - (Mathf.SmoothStep(0, 100, updateTime / coolTimedownTime) / 100);
+
+            if(60 - (int)(updateTime) == 0)
+            {
+                coolTimeText.text = "STOP";
+                coolTimeText.color = Color.red;
+            }
+            else
+            {
+                coolTimeText.text = (60 - (int)(updateTime)).ToString();
+            }
+
+            if (updateTime > coolTimedownTime)
+            {
+                coolTimeSlider.fillAmount = 0.0f;
+                updateTime = 0.0f;
+                canSlider = false;
+            }
+        }
+    }
+}
